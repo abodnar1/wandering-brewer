c0902cae3e86502deba817ad38145d463469672e
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-03 19:52:27 -0400

    Build Form component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,11 +2,13 @@ import React from "react";
 // import { Route } from "react-router-dom";
 import "./App.css";
 import Header from "../Header/Header";
+import Form from "../Form/Form";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Form />
     </div>
   );
 };
