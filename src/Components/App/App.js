4eb34590122c19fd7a3700495b45c8edb74a8b57
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-03 12:31:32 -0400

    Add Components folder; remove unnecessary files

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>The Wandering Brewer</h1>
+    </div>
+  );
+};
+
+export default App;
