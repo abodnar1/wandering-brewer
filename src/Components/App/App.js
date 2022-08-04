1258337067554140cfa5ac6efc0d545ba8bb7ec6
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-03 20:01:46 -0400

    Build Breweries container component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Form from "../Form/Form";
+import Breweries from "../Breweries/Breweries";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Form />
+      <Breweries />
     </div>
   );
 };
---
+++ src/Components/Breweries/Breweries.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Breweries.css";
+
+const Breweries = () => {
+  return (
+    <div className="breweries-container">
+      <h2>HIIIIIIII I'M A BREWERY CONTAINER</h2>
+    </div>
+  );
+};
+
+export default Breweries;
