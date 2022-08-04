643ca39ec3f265570af78d47fae0363f52607dd9
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-04 12:43:47 -0400

    Setup fetch request in useEffect in App.js using city of Savannah

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import { Route } from "react-router-dom";
 import "./App.css";
-import { mockDataSavannah } from "../../mockDataSavannah";
+// import { mockDataSavannah } from "../../mockDataSavannah";
 import Header from "../Header/Header";
 import Form from "../Form/Form";
 import Breweries from "../Breweries/Breweries";
 import BreweryDetails from "../BreweryDetails/BreweryDetails";
 
 const App = () => {
-  const [breweries, setBreweries] = useState(mockDataSavannah)
-  console.log(mockDataSavannah);
+  const [breweries, setBreweries] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.openbrewerydb.org/breweries?by_city=savannah")
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        return setErrorMessage("There was an error, please try again")
+      }
+    })
+    .then(data => setBreweries(data))
+    .catch(err => setErrorMessage(err.message))
+  }, []);
 
   return (
     <div className="App">
