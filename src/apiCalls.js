eaf33256ec118108605625e5cc41a1836e82c329
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-07 15:07:11 -0400

    Remove unnecessary fetchBreweryById

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,15 +9,4 @@ const fetchBreweriesByCity = (city) => {
   });
 };
 
-// const fetchBreweryById = (id) => {
-//   return fetch(`https://api.openbrewerydb.org/breweries/${id}`)
-//   .then(res => {
-//     if (res.ok) {
-//       return res.json()
-//     } else {
-//       throw new Error("Oops! Something went wrong.")
-//     }
-//   })
-// }
-
 export { fetchBreweriesByCity };
