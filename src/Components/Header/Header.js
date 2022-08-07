488bdd6927ddf31364f65ccc445283279541195a
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-07 12:56:45 -0400

    Remove unused logo assets

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/black-on-white-transpar-logo.png";
 import "./Header.css";
 
 const Header = () => {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
+  // const date = new Date();
+  // const hours = date.getHours();
+  // let timeOfDay;
 
-  const getTimeOfDay = () => {
-    if (hours < 12) {
-      timeOfDay = "morning";
-    } else if (hours >= 12 && hours < 17) {
-      timeOfDay = "afternoon";
-    } else {
-      timeOfDay = "evening";
-    };
-  };
+  // const getTimeOfDay = () => {
+  //   if (hours < 12) {
+  //     timeOfDay = "morning";
+  //   } else if (hours >= 12 && hours < 17) {
+  //     timeOfDay = "afternoon";
+  //   } else {
+  //     timeOfDay = "evening";
+  //   };
+  // };
 
-  getTimeOfDay();
+  // getTimeOfDay();
 
   return (
     <header className="header">
       <Link to="/" style={{textDecoration: "none"}}>
-        <h1 className="app-title">Wandering Brewer</h1>
+        <img className="logo-home" src={logo} alt="Wandering Brewer logo"></img>
+        {/* <h1 className="app-title">Wandering Brewer</h1> */}
       </Link>
-      <p className="greeting">Good {timeOfDay}!</p>
+      {/* <p className="greeting">Good {timeOfDay}!</p> */}
       <Link to="/favorites" style={{textDecoration: "none"}}>
         <p className="favorites-link">Favorites</p>
       </Link>
