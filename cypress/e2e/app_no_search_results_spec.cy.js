56e81e094b62000b9ff1c05a52cbbee02f80bece
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-07 11:22:18 -0400

    Add tests for Main Page no search results and Main Page with search results

---
+++ cypress/e2e/app_no_search_results_spec.cy.js
@@ -0,0 +1,16 @@
+describe("Wandering Brewer Main Page Without Search Results", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/")
+  });
+
+  it("should have a header with a title", () => {
+    cy.get(".app-title").contains("h1", "Wandering Brewer")
+    .get(".no-search-results-message").contains("h3", "Choose a city to search!")
+  });
+
+  it("should have a header with a link to view favorites", () => {
+    cy.get(".favorites-link").click()
+    .url().should("eq", "http://localhost:3000/favorites")
+    .get(".no-favorites-message").contains("h3", "You haven't added any favorites yet!")
+  });
+}); 
---
+++ cypress/e2e/app_search_results_spec.cy.js
@@ -0,0 +1,34 @@
+describe("Wandering Brew Main Page With Search Results", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://api.openbrewerydb.org/breweries?by_city=savannah", {
+      statusCode: 200,
+      fixture: "savannah_breweries"
+    })
+    .visit("http://localhost:3000/")
+    // how to run the test for cleaning data?
+  });
+
+  it("should have a form to enter a city and display that city's breweries", () => {
+    cy.get("input[name='city']").type("savannah")
+    .get(".search-button").click()
+    .get(".breweries-container").find(".brewery-card-wrapper").should("have.length", 2)
+    .get(".brewery-name").contains("h2", "Moon River Brewing Co")
+    .get(".brewery-name").contains("h2", "Two Tides Brewing Company")
+    .get(".brewery-location").first().contains("p", "Savannah, Georgia")
+  })
+  
+  it("should be able to click on the 'view details' link and see more information about the brewery", () => {
+    cy.get("input[name='city']").type("savannah")
+    .get(".search-button").click()
+    .get(".breweries-container").find(".brewery-card-wrapper").should("have.length", 2)
+    
+    .get(".view-details-link").first().click()
+    .url().should("eq","http://localhost:3000/moon-river-brewing-co-savannah")
+    .get(".brewery-details-name").contains("h2", "Moon River Brewing Co")
+    .get(".brewery-type").contains("p", "brewpub")
+    .get(".phone").contains("a", "Phone: 912-447-0943")
+    .get(".website-link").contains("a", "Visit website")
+    .get(".back-to-search-link").contains("span", "back to search").click()
+    .url().should("eq", "http://localhost:3000/")    
+  });
+});
