e0698b2621ecdf9356d4dd33f61cb70cb38d421b
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-07 13:51:51 -0400

    Add styling to view details link hover

--- src/Components/BreweryCard/BreweryCard.js
+++ src/Components/BreweryCard/BreweryCard.js
@@ -39,14 +39,9 @@ const BreweryCard = ({ id, name, city, state, type, isFavorite, favorites, setFa
     <div className="brewery-card-wrapper">
       <h2 className="brewery-name">{name}</h2>
       <p className="brewery-location">{city}, {state}</p>
-      {/* <div className="brewery-type"> */}
-        {/* <p className="brewery-type">Type: {type}</p>
-      </div> */}
       {favoriteIcon ? <span className="favorite-icon" onClick={(e) => removeFavorite(e, id)}><FaBeer className="full-beer-icon"/></span> 
       : 
       <span className="favorite-icon" onClick={(e) => addFavorite(e)}><BiBeer className="empty-beer-icon"/></span>}
-      {/* <span className="favorite-icon" onClick={(e) => addFavorite(e)}><AiOutlineHeart className="add-heart"/></span> */}
-      {/* <button className="add-favorite-button" disabled={isFavorite} onClick={(e) => addFavorite(e)}>Add to Favorites</button> */}
       <Link to={`/${id}`} style={{textDecoration: "none"}}>
        <p className="view-details-link">View details</p>
       </Link>
