34416b603287dd01fcf7017c32803a3c38a5e5db
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-07 08:51:14 -0400

    Change Brewery card to only allow favorite one time

--- src/Components/BreweryCard/BreweryCard.js
+++ src/Components/BreweryCard/BreweryCard.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { BiBeer } from "react-icons/bi";
+import { FaBeer } from 'react-icons/fa';
 import "./BreweryCard.css";
 
 const BreweryCard = ({ id, name, city, state, type, isFavorite, favorites, setFavorites }) => {
+  const [favoriteIcon, setFavoriteIcon] = useState(false);
+
+  useEffect(() => {
+    setFavoriteIcon(favorites.find(favoriteCard => favoriteCard.id === id))
+  }, [favorites, id]);
 
   const addFavorite = (e) => {
     e.preventDefault();
@@ -16,8 +23,19 @@ const BreweryCard = ({ id, name, city, state, type, isFavorite, favorites, setFa
       isFavorite: true,
       setFavorites
     };
+    setFavoriteIcon(true)
     setFavorites([...favorites, newFavorite]);
   };
+
+  const removeFavorite = (e, id) => {
+    e.preventDefault();
+    isFavorite = false;
+    setFavoriteIcon(false)
+    const updatedFavorites = favorites.filter(favorite => favorite.id !== id);
+    setFavorites(updatedFavorites);
+  };
+
+
 
   return (
     <Link to={`/${id}`} style={{textDecoration: "none"}}>
@@ -27,7 +45,11 @@ const BreweryCard = ({ id, name, city, state, type, isFavorite, favorites, setFa
         {/* <div className="brewery-type"> */}
           {/* <p className="brewery-type">Type: {type}</p>
         </div> */}
-        <button className="add-favorite-button" disabled={isFavorite} onClick={(e) => addFavorite(e)}>Add to Favorites</button>
+        {favoriteIcon ? <span className="favorite-icon" onClick={(e) => removeFavorite(e, id)}><FaBeer className="full-beer-icon"/></span> 
+				: 
+				<span className="favorite-icon" onClick={(e) => addFavorite(e)}><BiBeer className="empty-beer-icon"/></span>}
+        {/* <span className="favorite-icon" onClick={(e) => addFavorite(e)}><AiOutlineHeart className="add-heart"/></span> */}
+        {/* <button className="add-favorite-button" disabled={isFavorite} onClick={(e) => addFavorite(e)}>Add to Favorites</button> */}
       </div>
     </Link>
   );
