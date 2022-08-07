f211316e4fc36b3130bfedcd1051e2fc6515e0ba
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-06 20:03:35 -0400

    Add assets

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,26 +3,29 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
-
   const date = new Date();
   const hours = date.getHours();
   let timeOfDay;
 
-  if (hours < 12) {
-    timeOfDay = "morning";
-  } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon";
-  } else {
-    timeOfDay = "evening";
+  const getTimeOfDay = () => {
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "evening";
+    };
   };
+
+  getTimeOfDay();
 
   return (
     <header className="header">
-      <Link to="/">
+      <Link to="/" style={{textDecoration: "none"}}>
         <h1 className="app-title">Wandering Brewer</h1>
       </Link>
       <p className="greeting">Good {timeOfDay}!</p>
-      <Link to="/favorites">
+      <Link to="/favorites" style={{textDecoration: "none"}}>
         <p className="favorites-link">Favorites</p>
       </Link>
     </header>
@@ -30,5 +33,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// will need to wrap h1 in a Link without textDecoration for "/"
